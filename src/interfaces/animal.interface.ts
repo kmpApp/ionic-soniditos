c79703a0703874564d69b520ac4fcb19f2dce7ae
Diego Campana <diegogcampana@hotmail.com>
2018-01-26 12:34:03 -0600

    se agrega list y recursos, tambien se agrega la reproduccion de sonidos

---
+++ src/interfaces/animal.interface.ts
@@ -0,0 +1,11 @@
+
+
+
+export interface Animal{
+  nombre:string;
+  imagen:string;
+  audio:string
+  duracion:number;
+  reproduciendo:boolean;
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+
+//se importa el archivo
+import { ANIMALES }from '../../data/data.animales';
+
+//import interface
+import { Animal }from '../../interfaces/animal.interface';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +14,53 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales:Animal[] = [];
+  audio = new Audio();
+  audioTiempo:any;
 
+//constructor
+  constructor(){
+    this.animales = ANIMALES.slice(0);
   }
+
+//Reproducir Sonido
+  private reproducir(a:Animal){
+
+    this.pausarSonido(a);
+
+    if(a.reproduciendo){
+      a.reproduciendo = false;
+      return;
+    }
+
+    this.audio.src = a.audio;
+
+    this.audio.load();
+    this.audio.play();
+
+    //bandera de reproduccion
+    a.reproduciendo = true;
+    //se cambia el estado de reproduciendo para cambiar los iconos
+    this.audioTiempo = setTimeout( ()=> a.reproduciendo =false, a.duracion * 1000  );
+  }
+
+ //Pausar Sonido
+   private pausarSonido(animalSeleccionado:Animal){
+     clearTimeout(this.audioTiempo);
+
+     this.audio.pause();
+     this.audio.currentTime = 0;
+     animalSeleccionado.reproduciendo = false;
+
+     for (let animal of this.animales) {
+         if(animal.nombre != animalSeleccionado.nombre){
+           animal.reproduciendo = false;
+         }
+     }
+
+
+   }
+
+
 
 }
